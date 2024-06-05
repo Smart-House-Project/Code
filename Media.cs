bea8424584fc59b4356463864c22868bcd4301d6
AUEPUTIN <144217319+AUEPUTIN@users.noreply.github.com>
2024-06-05 21:31:42 +0300

    Add files via upload

---
+++ Media.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SmartHouseProject
+{
+    public class Media
+    {
+        private Dictionary<int, boolean> deviceStatus;
+        private Dictionary<int, Content> currentContent;
+        private MediaLibrary mediaLibrary;
+        private Dictionary<int, Schedule> playbackSchedule;
+        private bool assistantIntegrationStatus;
+
+        public void ControlDevice()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void StreamContent()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void ManageLibrary()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void SchedulePlayback()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void IntegrateWithAssistant()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Service service;
+
+    }
+}

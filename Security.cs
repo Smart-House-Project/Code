6986bc327d73cc6188bdd19f77324510e3bcdd6e
AUEPUTIN <144217319+AUEPUTIN@users.noreply.github.com>
2024-06-11 12:11:00 +0300

    Security.cs

--- Security.cs
+++ Security.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,24 +6,36 @@ using System.Threading.Tasks;
 
 namespace SmartHouseProject
 {
+    /// <summary>
+    /// Клас Security представляє систему безпеки, яка включає функції 
+    /// озброєння та роззброєння сигналізації, моніторингу камер, 
+    /// контролю доступу та сповіщення влади.
+    /// </summary>
     public class Security
     {
-        bool burglarAlarmStatus;
-        private bool alarmStatus;
-
+        bool burglarAlarmStatus;// Статус сигналізації від зломів
+        private bool alarmStatus;// Загальний статус сигналізації
+        /// <summary>
+        /// Вмикає сигналізацію від зломів.
+        /// </summary>
         public void ArmAlarm()
         {
             Console.WriteLine("Burglar alarm is on");
             burglarAlarmStatus = true;
             throw new NotImplementedException();
         }
-
+        /// <summary>
+        /// Вимикає сигналізацію від зломів.
+        /// </summary>
         public void DisarmAlarm()
         {
             Console.WriteLine("Burglar alarm is off");
             burglarAlarmStatus = false;
             throw new NotImplementedException();
         }
+        /// <summary>
+        /// Вмикає загальну сигналізацію та сповіщає про злом.
+        /// </summary>
         public void TurnOnAlarm()
         {
             alarmStatus = true;
@@ -32,12 +44,17 @@ namespace SmartHouseProject
             SendAlerts();
             throw new NotImplementedException();
         }
+        /// <summary>
+        /// Моніторить камери спостереження.
+        /// </summary>
         public void MonitorCameras()
         {
             Console.WriteLine("Monitoring the house...");
             throw new NotImplementedException();
         }
-
+        /// <summary>
+        /// Сповіщає про злом.
+        /// </summary>
         public void NotifyAuthorities()
         {
             Console.WriteLine("Calling the police...");
@@ -49,7 +66,9 @@ namespace SmartHouseProject
         {
             throw new NotImplementedException();
         }
-
+        /// <summary>
+        /// Надсилає сповіщення про тривогу.
+        /// </summary>
         public void SendAlerts()
         {
             if(alarmStatus)
